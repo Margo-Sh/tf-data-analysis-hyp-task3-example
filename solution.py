a733344a572fed022eaefe36fd3bb1632d1acb4e
Margo-Sh <67891965+Margo-Sh@users.noreply.github.com>
2024-05-23 15:04:26 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,19 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 334982039 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x, y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.02
+
+    x_mean = np.mean(x)
+    y_mean = np.mean(y)
+    x_var = np.var(x)
+    y_var = np.var(y)
+
+    std = np.sqrt(x_var / x.size + y_var / y.size)
+    z_stat = sps.norm.ppf(1-alpha)
+    return y_mean - x_mean > std * z_stat # Ваш ответ, True или False
